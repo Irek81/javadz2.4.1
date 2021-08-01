4eba1ac97cf31c160f2db04de8c997f227b82c52
Irek81 <irekisanbirdin@gmail.com>
2021-08-01 22:13:03 +0500

    initial commit

---
+++ src/main/java/ru/netology/services/MyService.java
@@ -0,0 +1,58 @@
+package ru.netology.services;
+
+public class MyService {
+
+    public int sumAllSales(int[] sales) {
+        int sum = 0;
+        for (int i = 0; i < sales.length; i++) {
+            sum += sales[i];
+        }
+        return sum;
+    }
+
+    public int average(int[] sales) {
+        int average = 0;
+        if (sales.length > 0) {
+            int sum = 0;
+            for (int j = 0; j < sales.length; j++) {
+                sum += sales[j];
+            }
+            average = sum / sales.length;
+        }
+        return average;
+    }
+
+    public int maxSales(int[] sales) {
+        int maxMonth = 0;
+        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+        for (long sale : sales) {
+            // sales[minMonth] - продажи в месяце minMonth
+            // sale - продажи в рассматриваемом месяце
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+        }
+        return maxMonth + 1;
+    }
+
+    public int minSales(int[] sales) {
+        int minMonth = 0;
+        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+        for (long sale : sales) {
+            // sales[minMonth] - продажи в месяце minMonth
+            // sale - продажи в рассматриваемом месяце
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+        }
+        return minMonth + 1;
+    }
+
+    public int monthUnderAverage (int[] sales) {
+        int mAU = 0;
+        int i = 0;
+    }
+
+}
---
+++ src/test/java/ru/netology/services/MyServiceTest.java
@@ -0,0 +1,49 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MyServiceTest {
+
+    @Test
+    public void shouldSumAllSales() {
+        MyService service = new MyService();
+        int [] sales = { 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18 };
+        int actual = service.sumAllSales(sales);
+        int expected = 180;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAverage() {
+        MyService servic = new MyService();
+        int [] sales = { 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18 };
+        int actual = servic.average(sales);
+        int expected = 15;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldMaxMonth() {
+        MyService service = new MyService();
+        int[] sales = { 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18 };
+        int actual = service.maxSales(sales);
+        int expected = 8;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldMinMonth() {
+        MyService service = new MyService();
+        int[] sales = { 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18 };
+        int actual = service.minSales(sales);
+        int expected = 9;
+        assertEquals(expected, actual);
+    }
+
+
+}
